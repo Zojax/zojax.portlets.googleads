0c77f41a9824ada0160a1763804ff6bbfea7fa78
Bogdan Girman <bogdan.girman@gmail.com>
2011-12-07 22:06:18 +0200

    #204 Fixed translation. Fixed missed tags. Fixed i18n domain

--- src/zojax/portlets/googleads/interfaces.py
+++ src/zojax/portlets/googleads/interfaces.py
@@ -18,7 +18,7 @@ $Id$
 from zope import schema, interface
 from zope.i18nmessageid import MessageFactory
 
-_ = MessageFactory('zojax.portlets')
+_ = MessageFactory('zojax.portlets.googleads')
 
 
 class IGoogleAdsPortlet(interface.Interface):
